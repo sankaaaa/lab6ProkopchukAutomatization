75ed756096cffef36242c874c75c93e496572ff0
sankaaaa <olexa2404@gmail.com>
2024-06-24 22:23:14 +0300

    lab 6 done

---
+++ src/test/java/org/sasha/CucumberRunTest.java
@@ -0,0 +1,11 @@
+package org.sasha;
+
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
+public class CucumberRunTest {
+}
